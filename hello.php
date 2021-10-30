86050fe02a189d7aefad50e2ac1cd46c2199d12d
Duc Ngo Minh <Di_ju_si@gmx.de>
2021-10-30 21:52:42 +0200

    Ducs Änderung

--- hello.php
+++ hello.php
@@ -4,7 +4,7 @@
  * @version 1.1.0
  */
 /*
-Plugin Name: MeinVerein24
+Plugin Name: Hello_Dolly
 Plugin URI: http://wordpress.org/plugins/hello-dolly/
 Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
 Author: Kevin Beeskow, Hanning Liu, Tilo Karczewski, Duc Ngo Minh
---
+++ meinverein24.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Hello_Dolly
+ * @version 1.1.0
+ */
+/*
+Plugin Name: MeinVerein24
+Plugin URI: http://wordpress.org/plugins/hello-dolly/
+Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+Author: Kevin Beeskow, Hanning Liu, Tilo Karczewski, Duc Ngo Minh
+Version: 1.1.0
+Author URI: http://ma.tt/
+*/
